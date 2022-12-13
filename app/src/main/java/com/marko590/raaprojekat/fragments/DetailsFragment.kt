73585c8f5d13d86b2b91a81cb7b30f243780eded
Marko590 <markobojkovicmb@gmail.com>
2022-12-13 18:07:10 +0100

    Deleted unnecessary files

--- app/src/main/java/com/marko590/raaprojekat/fragments/DetailsFragment.kt
+++ app/src/main/java/com/marko590/raaprojekat/fragments/DetailsFragment.kt
@@ -1,6 +1,5 @@
 package com.marko590.raaprojekat.fragments
 
-import android.R.attr.data
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,6 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.marko590.raaprojekat.R
-import com.marko590.raaprojekat.adapter.DessertAdapter
 import com.marko590.raaprojekat.databinding.FragmentDetailsBinding
 import com.marko590.raaprojekat.models.ApiActivity
 
